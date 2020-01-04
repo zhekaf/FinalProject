b534c552db21cc76985de0e20c9b8efe8b727b7d
zhekaf <a842002@gmail.com>
2020-01-04 03:07:33 +0200

    first prototype

---
+++ Assets/Move.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float speed = 1.5f;
+    private Vector3 target;
+    private float MouseX;
+    private bool isMoving = false;
+    private float playerDistSizeX;
+    private float playerDistSizeY;
+    private float playerDistSizeZ;
+    private Vector3 playerZise;
+    public Animator animator;
+   
+
+    void Start()
+    {
+        playerZise = transform.localScale;
+        
+        //target = transform.position;
+        // MouseX = transform.position.x;
+    }
+
+    void Update()
+    {
+         if (Input.GetMouseButtonDown(0))
+          {
+            // MouseX = Input.GetAxis("Mouse X");
+            //target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            setTargetPosition();
+          }
+        //transform.position.x = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        /* target = new Vector2(MouseX, transform.position.y);
+
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);*/
+
+        
+
+         if (isMoving)
+          {
+             moveTo();
+            animator.SetFloat("PlayerSpeed", 1);
+          }
+          target.y = transform.position.y;
+          target.z = transform.position.z;
+          playerDistSizeX = playerZise.x + transform.position.x / 50;
+          playerDistSizeY = playerZise.y + transform.position.x / 50;
+          playerDistSizeZ = playerZise.z + transform.position.x / 50;
+
+          //transform.localScale.x = playerDistSizeX;
+
+          transform.localScale = new Vector3(playerDistSizeX, playerDistSizeY, playerDistSizeZ);
+    }
+
+    void setTargetPosition()
+    {
+        target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        
+        isMoving = true;
+    }
+
+    void moveTo()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        if(target.x < transform.position.x + 0.2 && target.x > transform.position.x - 0.2)
+        {
+            isMoving = false;
+            animator.SetFloat("PlayerSpeed", 0);
+
+        }
+
+    }
+}
